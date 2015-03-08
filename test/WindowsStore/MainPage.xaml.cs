fcd10cbc038027e07d47ac788869ea42b6390953
Yufei Huang <yufeih@live.com>
2015-03-08 19:14:27 +0800

    Add reference to xunit.runner.utility

--- test/WindowsStore/MainPage.xaml.cs
+++ test/WindowsStore/MainPage.xaml.cs
@@ -1,18 +1,28 @@
 ﻿namespace Nine.Application.WindowsStore.Test
 {
+    using System;
+    using System.Reflection;
     using Windows.UI.Xaml.Controls;
+    using Xunit.Abstractions;
+    using Xunit.Sdk;
 
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, IMessageSink
     {
         public MainPage()
         {
             InitializeComponent();
 
-            Loaded += async (a, b) =>
+            Loaded += (a, b) =>
             {
-                var test = new AppUITest();
-                await test.confirm(new AppUI());
+                var assembly = typeof(AppUITest).GetTypeInfo().Assembly;
+                var framework = new XunitTestFramework(this);
+                framework.GetExecutor(assembly.GetName()).RunAll(this, null, null);
             };
+        }
+
+        public bool OnMessage(IMessageSinkMessage message)
+        {
+            return true;
         }
     }
 }
