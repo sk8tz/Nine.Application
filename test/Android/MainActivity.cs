b372d79347cfaaf159bb676cb7343e9b2d3fe40a
Yufei Huang <yufeih@live.com>
2015-03-08 19:35:42 +0800

    Run all xunit test cases on android

--- test/Android/MainActivity.cs
+++ test/Android/MainActivity.cs
@@ -1,21 +1,31 @@
 ﻿using Android.App;
 using Android.OS;
+using Xunit;
+using Xunit.Sdk;
+using Xunit.Abstractions;
 
 namespace Nine.Application.Android.Test
 {
     [Activity(Label = "Nine.Application.Android.Test", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, IMessageSink
     {
-        protected async override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle bundle)
         {
             ContextProvider.Current = () => this;
 
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
+            
+            var test = new XunitTestFramework(this);
+            var executor = test.GetExecutor(typeof(AppUITest).Assembly.GetName());
+            executor.RunAll(this, TestFrameworkOptions.ForDiscovery(), TestFrameworkOptions.ForExecution());
+        }
 
-            var test = new AppUITest();
-            await test.confirm(new AppUI(this));
+        public bool OnMessage(IMessageSinkMessage message)
+        {
+            Title = message.ToString();
+            return true;
         }
     }
 }
