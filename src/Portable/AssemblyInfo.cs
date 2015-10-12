44bcbc04c6df4cba6ea958da9ceb0d92cb17cc60
Yufei Huang <yufeih@live.com>
2015-10-12 15:45:26 +0800

    [nuget] 0.1.3

--- src/Portable/AssemblyInfo.cs
+++ src/Portable/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Studio Nine 2015")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
