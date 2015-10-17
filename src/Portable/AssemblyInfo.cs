c3db1a2fa976b8d72657ebad98d6bf154ca7e1a1
Yufei Huang <yufeih@live.com>
2015-10-17 15:01:01 +0800

    Fix android audio record and playback memory leak. Fix android audio capture not capturing audios in last second.

--- src/Portable/AssemblyInfo.cs
+++ src/Portable/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Studio Nine 2015")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.1.3.0")]
-[assembly: AssemblyFileVersion("0.1.3.0")]
+[assembly: AssemblyVersion("0.1.4.0")]
+[assembly: AssemblyFileVersion("0.1.4.0")]
