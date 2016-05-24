4e9c507c3da584a2fd82bab0b3a771a943e551fb
Yufei Huang <yufeih@live.com>
2016-05-24 17:46:18 +0800

    Show in app notification on windows when not active

--- src/Windows/AppUI.cs
+++ src/Windows/AppUI.cs
@@ -28,6 +28,7 @@
     public partial class AppUI : IAppUI
     {
         private readonly SemaphoreSlim _toastQueue = new SemaphoreSlim(1);
+        private bool _showingInAppNotification;
         private bool _isWindowActive;
 
         public AppUI()
@@ -56,6 +57,89 @@
             }
         }
 
+        private async Task<bool> InAppNotify(string title, string message, CancellationToken cancellation)
+        {
+            if (_showingInAppNotification || cancellation.IsCancellationRequested) return false;
+
+            _showingInAppNotification = true;
+
+            var grid = FindChild<Grid>(Window.Current.Content);
+            if (grid == null)
+            {
+                _showingInAppNotification = false;
+                return false;
+            }
+
+            try
+            {
+                var text = string.Join(": ", new[] { title, message }.Where(str => !string.IsNullOrEmpty(str)));
+
+                var content = new Border
+                {
+                    BorderThickness = new Thickness(0),
+                    Background = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0)),
+                    Margin = new Thickness(0),
+                    Padding = new Thickness(12, 18, 12, 18),
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    VerticalAlignment = VerticalAlignment.Top,
+                };
+
+                content.Child = new TextBlock
+                {
+                    Text = text,
+                    MaxLines = 2,
+                    MaxWidth = Math.Min(grid.ActualWidth * 0.8, 320),
+                    TextWrapping = TextWrapping.Wrap,
+                    TextTrimming = TextTrimming.CharacterEllipsis,
+                    Foreground = new SolidColorBrush(Colors.White)
+                };
+
+                if (grid.RowDefinitions.Count > 0) Grid.SetRowSpan(content, grid.RowDefinitions.Count);
+                if (grid.ColumnDefinitions.Count > 0) Grid.SetColumnSpan(content, grid.ColumnDefinitions.Count);
+
+                grid.Children.Add(content);
+                content.Opacity = 0;
+
+                var fadeIn = new Storyboard();
+                var fadeInAnim = new DoubleAnimation { From = 0, To = 1, Duration = TimeSpan.FromSeconds(0.2) };
+                fadeIn.Children.Add(fadeInAnim);
+                Storyboard.SetTarget(fadeInAnim, content);
+                Storyboard.SetTargetProperty(fadeInAnim, "Opacity");
+                fadeIn.Begin();
+
+                var result = false;
+
+                if (!cancellation.IsCancellationRequested)
+                {
+                    var cts = new TaskCompletionSource<bool>();
+                    cancellation.Register(() => cts.TrySetResult(false));
+                    content.PointerPressed += (sender, e) => { result = true; cts.TrySetResult(true); };
+                    await Task.WhenAny(Task.Delay(TimeSpan.FromSeconds(4 - 0.2)), cts.Task);
+                }
+
+                var fade = new Func<Task>(async () =>
+                {
+                    var fadeOut = new Storyboard();
+                    var fadeOutAnim = new DoubleAnimation { To = 0, Duration = TimeSpan.FromSeconds(0.2) };
+                    fadeOut.Children.Add(fadeOutAnim);
+                    Storyboard.SetTarget(fadeOutAnim, content);
+                    Storyboard.SetTargetProperty(fadeOutAnim, "Opacity");
+                    fadeOut.Begin();
+
+                    await Task.Delay(TimeSpan.FromSeconds(0.2));
+
+                    grid.Children.Remove(content);
+                });
+
+                fade();
+                return result;
+            }
+            finally
+            {
+                _showingInAppNotification = false;
+            }
+        }
+
         public async void Toast(string title, string message)
         {
             await _toastQueue.WaitAsync();
@@ -67,56 +151,61 @@
                 return;
             }
 
-            var text = string.Join(": ", new[] { title, message }.Where(str => !string.IsNullOrEmpty(str)));
-
-            var content = new Border
+            try
             {
-                BorderThickness = new Thickness(0),
-                Background = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0)),
-                Margin = new Thickness(0, 0, 0, 24),
-                Padding = new Thickness(12, 8, 12, 8),
-                CornerRadius = new CornerRadius(12),
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Bottom,
-            };
+                var text = string.Join(": ", new[] { title, message }.Where(str => !string.IsNullOrEmpty(str)));
 
-            content.Child = new TextBlock
+                var content = new Border
+                {
+                    BorderThickness = new Thickness(0),
+                    Background = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0)),
+                    Margin = new Thickness(0, 0, 0, 24),
+                    Padding = new Thickness(12, 8, 12, 8),
+                    CornerRadius = new CornerRadius(12),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Bottom,
+                };
+
+                content.Child = new TextBlock
+                {
+                    Text = text,
+                    MaxLines = 2,
+                    MaxWidth = Math.Min(grid.ActualWidth * 0.8, 320),
+                    TextWrapping = TextWrapping.Wrap,
+                    TextTrimming = TextTrimming.CharacterEllipsis,
+                    Foreground = new SolidColorBrush(Colors.White)
+                };
+
+                if (grid.RowDefinitions.Count > 0) Grid.SetRowSpan(content, grid.RowDefinitions.Count);
+                if (grid.ColumnDefinitions.Count > 0) Grid.SetColumnSpan(content, grid.ColumnDefinitions.Count);
+
+                grid.Children.Add(content);
+                content.Opacity = 0;
+
+                var fadeIn = new Storyboard();
+                var fadeInAnim = new DoubleAnimation { From = 0, To = 1, Duration = TimeSpan.FromSeconds(0.2) };
+                fadeIn.Children.Add(fadeInAnim);
+                Storyboard.SetTarget(fadeInAnim, content);
+                Storyboard.SetTargetProperty(fadeInAnim, "Opacity");
+                fadeIn.Begin();
+
+                await Task.Delay(TimeSpan.FromSeconds(4 - 0.2));
+
+                var fadeOut = new Storyboard();
+                var fadeOutAnim = new DoubleAnimation { To = 0, Duration = TimeSpan.FromSeconds(0.2) };
+                fadeOut.Children.Add(fadeOutAnim);
+                Storyboard.SetTarget(fadeOutAnim, content);
+                Storyboard.SetTargetProperty(fadeOutAnim, "Opacity");
+                fadeOut.Begin();
+
+                await Task.Delay(TimeSpan.FromSeconds(0.2));
+
+                grid.Children.Remove(content);
+            }
+            finally
             {
-                Text = text,
-                MaxLines = 2,
-                MaxWidth = Math.Min(grid.ActualWidth * 0.8, 320),
-                TextWrapping = TextWrapping.Wrap,
-                TextTrimming = TextTrimming.CharacterEllipsis,
-                Foreground = new SolidColorBrush(Colors.White)
-            };
-
-            if (grid.RowDefinitions.Count > 0) Grid.SetRowSpan(content, grid.RowDefinitions.Count);
-            if (grid.ColumnDefinitions.Count > 0) Grid.SetColumnSpan(content, grid.ColumnDefinitions.Count);
-
-            grid.Children.Add(content);
-            content.Opacity = 0;
-
-            var fadeIn = new Storyboard();
-            var fadeInAnim = new DoubleAnimation { From = 0, To = 1, Duration = TimeSpan.FromSeconds(0.2) };
-            fadeIn.Children.Add(fadeInAnim);
-            Storyboard.SetTarget(fadeInAnim, content);
-            Storyboard.SetTargetProperty(fadeInAnim, "Opacity");
-            fadeIn.Begin();
-
-            await Task.Delay(TimeSpan.FromSeconds(4 - 0.2));
-            
-            var fadeOut = new Storyboard();
-            var fadeOutAnim = new DoubleAnimation { To = 0, Duration = TimeSpan.FromSeconds(0.2) };
-            fadeOut.Children.Add(fadeOutAnim);
-            Storyboard.SetTarget(fadeOutAnim, content);
-            Storyboard.SetTargetProperty(fadeOutAnim, "Opacity");
-            fadeOut.Begin();
-
-            await Task.Delay(TimeSpan.FromSeconds(0.2));
-
-            grid.Children.Remove(content);
-
-            _toastQueue.Release();
+                _toastQueue.Release();
+            }
         }
 
         private static T FindChild<T>(DependencyObject obj) where T : DependencyObject
@@ -143,12 +232,11 @@
 
         public Task<bool> Notify(string title, string message, CancellationToken cancellation)
         {
-            // Don't show notification on Windows Desktop if the window is not active
             if (!ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
             {
                 if (_isWindowActive)
                 {
-                    return Task.FromResult(false);
+                    return InAppNotify(title, message, cancellation);
                 }
             }
 
